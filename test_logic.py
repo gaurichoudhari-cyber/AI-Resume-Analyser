893b3c81db69968afc78bd6708067fa3809e4a1a
Gauri Choudhari <gauri.choudhari@cumminscollege.in>
2026-04-22 15:12:47 +0530

    Update test_logic.py

--- test_logic.py
+++ test_logic.py
@@ -1,9 +1,9 @@
 def test_simple_check():
-    # Ye ek dummy test hai jo CI pipeline ko pass dikhayega
+    
     app_name = "AI Resume Analyser"
     assert "AI" in app_name
 
 def test_math_logic():
-    # Score calculation check karne ke liye
+   
     resume_score = 80
     assert resume_score >= 0 and resume_score <= 100
